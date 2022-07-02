104b7ffc4c724a453954c11b8eed27bee8027d7c
Rishabh Tripathi <86038495+Rishabh-Tripathi1@users.noreply.github.com>
2022-07-02 22:17:05 +0530

    Update BlackFriday.py

--- BlackFriday.py
+++ BlackFriday.py
@@ -1,25 +1,27 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
-svc = SVR()
+from sklearn.metrics import r2_score
 
-data = pd.read_csv('train.csv')
+svc = RandomForestRegressor()
+
+data = pd.read_csv('Train.csv')
+data = data.drop(["User_ID","Product_ID"],axis=1)
 
 data['Age']=(data['Age'].str.strip('+'))
 data['Stay_In_Current_City_Years']=(data['Stay_In_Current_City_Years'].str.strip('+').astype('float'))
 
-data['Product_Category_2'].fillna(data['Product_Category_2'].mean(),inplace=True)
-data['Product_Category_3'].fillna(data['Product_Category_3'].mean(),inplace=True)
+data['Product_Category_2'] =data['Product_Category_2'].fillna(0).astype('int64')
+data['Product_Category_3'] =data['Product_Category_3'].fillna(0).astype('int64')
 
 le = LabelEncoder()
 data['Gender'] = le.fit_transform(data['Gender'])
 data['Age'] = le.fit_transform(data['Age'])
 data['City_Category'] = le.fit_transform(data['City_Category'])
 
-x = data.drop(['Purchase','Product_ID','User_ID'], axis=1)
-x = x.astype('int')
+x = data.drop(['Purchase'], axis=1)
 y = data['Purchase']
 
 
@@ -31,3 +33,10 @@ y_pred = svc.predict(x_test)
 
 
 print(mean_squared_error(y_test,y_pred))
+print(r2_score(y_test, y_pred))
+
+
+'''
+R2 Score:
+0.632169133989062
+'''
